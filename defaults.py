30598628e5b05d6180194e59f8f1e1d35594491a
Dale Hamel <dale.hamel@srvthe.net>
2015-02-28 15:17:22 +0000

    Enable hyperion support

--- defaults.py
+++ defaults.py
@@ -78,6 +78,7 @@ services = {
     'D_LCD_DRIVER': 'none',
     'REMOTEPI_DAEMON': '/usr/bin/remotepi-board',
     'D_REMOTEPI_VERSION': '2015',
+    'HYPERION_DAEMON': '/usr/bin/hyperiond',
     }
 
 system = {
@@ -115,4 +116,5 @@ _services = {
     'crond': ['cron.service'],
     'lcdd': ['lcdd.service'],
     'remotepi-board': ['remotepi-board.service'],
+    'hyperion': ['hyperion.service'],
     }
